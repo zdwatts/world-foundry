316421a7b21594c6864fd35d7f2f0814fa3f06cb
zacwatts <zdwatts@gmail.com>
2021-01-05 15:12:50 -0700

    successfully seeded demo user

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -6,8 +6,24 @@ from flask_login import LoginManager
 
 from .config import Configuration
 from .models import db, User
+from .seeds import seed_commands
+
 
 app = Flask(__name__)
+
+
+login = LoginManager(app)
+login.login_view = "auth.unauthorized"
+
+
+@login.user_loader
+def load_user(id):
+	return User.query.get(int(id))
+
+
+app.cli.add_command(seed_commands)
+
+
 app.config.from_object(Configuration)
 db.init_app(app)
 migrate = Migrate(app, db)
