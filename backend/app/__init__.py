8879bc4965da055730454def317222805085c5a5
zacwatts <zdwatts@gmail.com>
2021-01-05 14:05:33 -0700

    started database setup

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,12 +1,15 @@
 import os
 from flask import Flask
 from flask_cors import CORS
-from flask_migrate import flask_migrate
+from flask_migrate import Migrate
 from flask_login import LoginManager
 
 from .config import Configuration
+from .models import db
 
 app = Flask(__name__)
-
+app.config.from_object(Configuration)
+db.init_app(app)
+Migrate(app, db)
 
 CORS(app)
