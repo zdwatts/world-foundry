7e503706fe23d5c32d16f9b5171f97f18604fb83
zacwatts <zdwatts@gmail.com>
2021-01-06 09:41:30 -0700

    wrote login route

--- backend/app/api/auth_routes.py
+++ backend/app/api/auth_routes.py
@@ -18,7 +18,7 @@ def validation_errors_to_error_messages(validation_errors):
     return errorMessages
 
 
-@auth.routes.route("/")
+@auth_routes.route("/")
 def authenticate():
     """
     Authenticates a user.
@@ -26,3 +26,21 @@ def authenticate():
     if current_user.is_authenticated:
         return current_user.to_dict()
     return {"errors": ["Unauthorized"]}, 401
+
+
+@auth_routes.route("/login", methods=["POST"])
+def login():
+    """
+    Logs a user in
+    """
+    form = LoginForm()
+    print(request.get_json())
+    # Get the csrf_token from the request cookie and put it into the
+    # form manually so validate_on_submit can be used
+    form["csrf_token"].data = request.cookies["csrf_token"]
+    if form.validate_on_submit():
+        # Add the user to the session, we are logged in!
+        user = User.query.filter(User.email == form.data["email"]).first()
+        login_user(user)
+        return user.to_dict()
+    return {"errors": validation_errors_to_error_messages(form.errors)}, 401
