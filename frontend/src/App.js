e3151a20fca79a8235734c004730d7eb22b57302
zacwatts <zdwatts@gmail.com>
2021-01-06 11:50:50 -0700

    installed react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
+// import { BrowserRouter, Route, Switch } from "react-router-dom";
+// import { authenticate } from "./services/auth";
 
 function App() {
-  return (
-    <h1>Hello world!</h1>
-  );
+	return <h1>Hello world!</h1>;
 }
 
 export default App;
