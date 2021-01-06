6475c1fb168250d7bcaee0009d5bc1c18fa0a105
zacwatts <zdwatts@gmail.com>
2021-01-06 10:46:20 -0700

    created document seeders

--- backend/app/seeds/__init__.py
+++ backend/app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .directories import seed_directories, undo_directories
+from .documents import seed_documents, undo_documents
 
 
 seed_commands = AppGroup("seed")
@@ -10,9 +11,11 @@ seed_commands = AppGroup("seed")
 def seed():
     seed_users()
     seed_directories()
+    seed_documents()
 
 
 @seed_commands.command("undo")
 def undo():
     undo_users()
     undo_directories()
+    undo_documents()
