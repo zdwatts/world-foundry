0afa41b57118579170633786c296bab6688cc71f
zacwatts <zdwatts@gmail.com>
2021-01-06 09:53:16 -0700

    added csrf token injector to app

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -7,6 +7,7 @@ from flask_login import LoginManager
 from .config import Configuration
 from .models import db, User
 from .seeds import seed_commands
+from .api.auth_routes import auth_routes
 
 
 app = Flask(__name__)
@@ -25,7 +26,21 @@ app.cli.add_command(seed_commands)
 
 
 app.config.from_object(Configuration)
+app.register_blueprint(user_routes, url_prefix="/api/users")
 db.init_app(app)
 migrate = Migrate(app, db)
 
 CORS(app)
+
+
+@app.after_request
+def inject_csrf_token(response):
+    response.set_cookie("csrf_token",
+                        generate_csrf().
+                        secure=True if os.environ.get(
+                            "FLASK_ENV") == "production" else False,
+                        samesite="Strict" if os.environ.get(
+                            "FLASK_ENV") == "production" else None,
+                        httponly=True
+                        )
+    return response
