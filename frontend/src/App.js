b7171c748f98f4218bf7ec1574eb628127d92b6d
zacwatts <zdwatts@gmail.com>
2021-01-06 11:58:48 -0700

    working on login form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-// import { BrowserRouter, Route, Switch } from "react-router-dom";
-// import { authenticate } from "./services/auth";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { authenticate } from "./services/auth";
 
 function App() {
 	return <h1>Hello world!</h1>;
---
+++ frontend/src/components/auth/LoginForm.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import { Redirect } from "react-router-dom";
+import { login } from "../../services/auth";
+
+const LoginForm = ({ authenticated, setAuthenticated }) => {
+	const [errors, setErrors] = useState([]);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const onLogin = async (e) => {
+		e.preventDefault();
+		const user = await login(email, password);
+		if (!user.errors) {
+			setAuthenticated(true);
+		} else {
+			setErrors(user.errors);
+		}
+	};
+
+	const updateEmail = (e) => {
+		setEmail(e.target.value);
+	};
+
+	const updatePassword = (e) => {
+		setPassword(e.target.value);
+	};
+
+	if (authenticated) {
+		return <Redirect to="/" />;
+	}
+
+	return (
+		<div>
+			<form onSubmit={onLogin}>
+				<div>
+					{errors.map((error) => (
+						<div>
+							<p>{error}</p>
+						</div>
+					))}
+				</div>
+				<div>
+					<label>Email</label>
+					<input
+						name="email"
+						type="text"
+						value={email}
+						onChange={updateEmail}
+					/>
+				</div>
+			</form>
+		</div>
+	);
+};
