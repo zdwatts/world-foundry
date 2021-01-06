d0c382ef939740a8b1106fd134055d9ef927b78f
zacwatts <zdwatts@gmail.com>
2021-01-06 09:37:57 -0700

    wrote first auth route

--- backend/app/api/auth_routes.py
+++ backend/app/api/auth_routes.py
@@ -1,5 +1,28 @@
 from flask import Blueprint
 from app.models import User, db
+from app.forms import LoginForm, SignUpForm
+from flask_login import current_user, login_user, logout_user, login_required
 
 
 auth_routes = Blueprint("auth", __name__)
+
+
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f"{field} : {error}")
+    return errorMessages
+
+
+@auth.routes.route("/")
+def authenticate():
+    """
+    Authenticates a user.
+    """
+    if current_user.is_authenticated:
+        return current_user.to_dict()
+    return {"errors": ["Unauthorized"]}, 401
