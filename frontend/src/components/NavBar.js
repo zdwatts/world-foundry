ca3e91270ba26ed9759fd110f751468b549855ef
zacwatts <zdwatts@gmail.com>
2021-01-07 14:37:56 -0700

    centered logo, working on centering navlinks

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -6,7 +6,7 @@ import "./styles/NavBar.css";
 
 const NavBar = ({ setAuthenticated, authenticated, authenticate }) => {
 	return (
-		<nav>
+		<nav className="navbar">
 			<div className="main-logo">
 				<a href="/" style={{ textDecoration: "none" }}>
 					<h1
@@ -19,35 +19,37 @@ const NavBar = ({ setAuthenticated, authenticated, authenticate }) => {
 					</h1>
 				</a>
 			</div>
-			<ul className="nav-links">
-				<li>
-					<NavLink exact to="/">
-						Home
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to="/login" activeClassName="active">
-						Log In
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to="/signup" activeClassName="active">
-						Sign Up
-					</NavLink>
-				</li>
-				<li>
-					<DemoButton
-						setAuthenticated={setAuthenticated}
-						authenticated={authenticated}
-					/>
-				</li>
-				<li>
-					<LogoutButton
-						setAuthenticated={setAuthenticated}
-						authenticated={authenticated}
-					/>
-				</li>
-			</ul>
+			<div className="links-wrapper">
+				<ul className="nav-links">
+					<li>
+						<NavLink exact to="/">
+							Home
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to="/login" activeClassName="active">
+							Log In
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to="/signup" activeClassName="active">
+							Sign Up
+						</NavLink>
+					</li>
+					<li>
+						<DemoButton
+							setAuthenticated={setAuthenticated}
+							authenticated={authenticated}
+						/>
+					</li>
+					<li>
+						<LogoutButton
+							setAuthenticated={setAuthenticated}
+							authenticated={authenticated}
+						/>
+					</li>
+				</ul>
+			</div>
 		</nav>
 	);
 };
