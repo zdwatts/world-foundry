32bd42a66e49c269d0e2f8dc0612971defaaf999
zacwatts <zdwatts@gmail.com>
2021-01-07 15:53:03 -0700

    added tinymce apikey to document editor

--- backend/app/api/document_routes.py
+++ backend/app/api/document_routes.py
@@ -1,12 +1,18 @@
-from flask import Blueprint
-from app.models import Document
+from flask import Blueprint, jsonify, request
+from flask_login import login_required, current_user
+from app.models import Document, db
 
 document_routes = Blueprint("documents", __name__)
 
 
 @document_routes.route("/")
-def document():
-    document = Document.query.filter_by(id=1).first()
-    print("TITLE:", document.title)
-    print("BODY:", document.body)
-    return {}
+def documents():
+    documents = Document.query.order_by(Document.id.desc())
+    return {"documents": [document.to_dict() for document in documents]}
+
+
+@document_routes.route("/", methods=["POST"])
+def new_document():
+    title = request.json["title"]
+    body = request.json["body"]
+    directory_id = Document.query.filter_by()
