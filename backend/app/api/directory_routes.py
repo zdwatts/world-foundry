be523d88e92586f1437e05f25285dee312556acc
zacwatts <zdwatts@gmail.com>
2021-01-08 11:13:57 -0700

    playing around with routes to get a valid json response

--- backend/app/api/directory_routes.py
+++ backend/app/api/directory_routes.py
@@ -1,16 +1,13 @@
-from flask import Blueprint
-from app.models import Directory
+from flask import Blueprint, jsonify
+from app.models import Directory, User, db, Document
+from flask_login import current_user, login_required
 
 
 directory_routes = Blueprint("directories", __name__)
 
 
 @directory_routes.route("/")
+@login_required
 def directory():
-    directory = Directory.query.filter_by(id=1).first()
-    print("1:", directory)
-    children = directory.children
-    print("2:", children)
-    parent = children[0].parent
-    print("3:", parent)
-    return {}
+    documents = Document.query.order_by(Document.id.desc())
+    return {"documents": [document.to_dict() for document in documents]}
