4daf33ad889bf22d05008bd6b467b7877775fb5c
zacwatts <zdwatts@gmail.com>
2021-01-08 11:33:52 -0700

    directory route now displays directory names when queried

--- backend/app/api/directory_routes.py
+++ backend/app/api/directory_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify
-from app.models import Directory, User, db, Document
+from app.models import Directory, User, db
 from flask_login import current_user, login_required
 
 
@@ -9,5 +9,12 @@ directory_routes = Blueprint("directories", __name__)
 @directory_routes.route("/")
 @login_required
 def directory():
-    documents = Document.query.order_by(Document.id.desc())
-    return {"documents": [document.to_dict() for document in documents]}
+    directories = Directory.query.filter_by(user_id=current_user.id).all()
+    # print("DIRECTORIES:", directories)
+    return {"directories": [directory.name for directory in directories]}
+    # print("1:", directory)
+    # children = directory.children
+    # print("2:", children)
+    # parent = children[0].parent
+    # print("3:", parent)
+    # return {}
