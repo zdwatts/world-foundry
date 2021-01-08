c738a498eeed655416ab96efb2858c2a1a1c5408
zacwatts <zdwatts@gmail.com>
2021-01-08 14:55:57 -0700

    installed material-ui packages

--- frontend/src/components/Directories.js
+++ frontend/src/components/Directories.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { makeStyles } from "@material-ui/core/styles";
+import TreeView from "@material-ui/lab/TreeView";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import TreeItem from "@material-ui/lab/TreeItem";
 
 const Directories = () => {
 	const [directories, setDirectories] = useState([]);
@@ -13,17 +18,37 @@ const Directories = () => {
 		})();
 	}, []);
 
+	const useStyles = makeStyles({
+		root: {
+			height: 110,
+			flexGrow: 1,
+			maxWidth: 400,
+		},
+	});
+
+	const classes = useStyles();
+
+	const renderTree = (directories) => (
+		<TreeItem
+			key={directories.id}
+			nodeId={directories.id}
+			label={directories.name}
+		>
+			{Array.isArray(directories.children)
+				? directories.children.map((directory) => renderTree(directory))
+				: null}
+		</TreeItem>
+	);
+
 	return (
-		<div>
-			<h1>Directories</h1>
-			<div>
-				<ul>
-					{directories.map((directory) => {
-						return <li key={directory.id}>{directory.name}</li>;
-					})}
-				</ul>
-			</div>
-		</div>
+		<TreeView
+			className={classes.root}
+			defaultCollapseIcon={<ExpandMoreIcon />}
+			defaultExpanded={["root"]}
+			defaultExpandIcon={<ChevronRightIcon />}
+		>
+			{renderTree(directories)}
+		</TreeView>
 	);
 };
 
