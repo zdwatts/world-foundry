01902a3271f97f9f7c5b9c39b6c04d20bb0403da
zacwatts <zdwatts@gmail.com>
2021-01-13 09:43:55 -0700

    added push request on form submit

--- frontend/src/components/DocumentEditor.js
+++ frontend/src/components/DocumentEditor.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import { Editor } from "@tinymce/tinymce-react";
+import axios from "axios";
 
 const DocumentEditor = ({ authenticate }) => {
 	const [title, setTitle] = useState("");
 	const [body, setBody] = useState("");
+	const history = useHistory();
 
 	const apiKey = process.env.REACT_APP_TINY_MCE;
 
@@ -15,10 +18,25 @@ const DocumentEditor = ({ authenticate }) => {
 		setBody(content);
 	};
 
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		const response = await axios.post("/api/documents/", {
+			title,
+			body,
+		});
+
+		if (response.ok) {
+			const data = await response.json();
+			const documentId = data.id;
+			history.push(`/documents/${documentId}`);
+		}
+	};
+
 	return (
 		<div className="document-wrapper">
 			<div>
-				<form>
+				<form onSubmit={handleSubmit}>
 					<div>
 						<label>Document Title</label>
 						<input type="text" name="title" required onChange={titleChange} />
