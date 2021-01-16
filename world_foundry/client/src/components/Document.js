e88c97c5e17e2a5a5d2c88a1ce5a747e2b1b5de1
zacwatts <zdwatts@gmail.com>
2021-01-15 17:57:35 -0700

    implemented ternary logic to show or hide editor with edit document button

--- world_foundry/client/src/components/Document.js
+++ world_foundry/client/src/components/Document.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
+import { Editor } from "@tinymce/tinymce-react";
 import axios from "axios";
 import parse from "html-react-parser";
 
 const Document = ({ authenticate }) => {
 	const [document, setDocument] = useState("");
+	const [showEditor, setShowEditor] = useState(false);
 	const history = useHistory();
 
 	const { id } = useParams();
+
+	const apiKey = process.env.REACT_APP_TINY_MCE;
 
 	useEffect(() => {
 		(async () => {
@@ -22,25 +26,39 @@ const Document = ({ authenticate }) => {
 		history.push("/directories");
 	};
 
-	const editDocument = async (id) => {};
-
 	// const editor;
 
 	return (
-		<div className="document-wrapper">
-			<div className="title-wrapper">
-				<h1>{document.title}</h1>
-			</div>
-			<div className="body-wrapper">
-				{document.body && parse(document.body)}
-			</div>
-			<div className="buttons-wrapper">
-				<button onClick={() => deleteDocument(document.id)}>
-					Delete Document
-				</button>
-				<button onClick={() => editDocument(document.id)}>Edit Document</button>
-			</div>
-		</div>
+		<>
+			{showEditor ? (
+				<div>
+					<form>
+						<Editor
+							apiKey={apiKey}
+							plugins="wordcount wordcount fullscreen image preview"
+						/>
+					</form>
+					<button onClick={() => setShowEditor(!showEditor)}>Cancel</button>
+				</div>
+			) : (
+				<div className="document-wrapper">
+					<div className="title-wrapper">
+						<h1>{document.title}</h1>
+					</div>
+					<div className="body-wrapper">
+						{document.body && parse(document.body)}
+					</div>
+					<div className="buttons-wrapper">
+						<button onClick={() => deleteDocument(document.id)}>
+							Delete Document
+						</button>
+						<button onClick={() => setShowEditor(!showEditor)}>
+							Edit Document
+						</button>
+					</div>
+				</div>
+			)}
+		</>
 	);
 };
 
