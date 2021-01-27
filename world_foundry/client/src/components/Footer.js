f9e5c4385e335b290ca1dc02e504baa648051ef1
zacwatts <zdwatts@gmail.com>
2021-01-26 17:28:40 -0700

    tried to change size of github icon

--- world_foundry/client/src/components/Footer.js
+++ world_foundry/client/src/components/Footer.js
@@ -3,17 +3,24 @@ import { NavLink } from "react-router-dom";
 
 const Footer = () => {
 	return (
-		<footer>
-			<div>
-				<h1>World Foundry</h1>
+		<footer className="footer">
+			<div className="main-logo">
+				<h1
+					className="logo-text"
+					style={{
+						fontFamily: "Permanent Marker, cursive",
+					}}
+				>
+					World Foundry
+				</h1>
 			</div>
-			<ul>
+			<ul className="nav-links">
 				<li>
 					<NavLink exact to="/" activeClassName="active">
 						<i class="fas fa-home"></i>
 					</NavLink>
 					<a href="https://github.com/zdwatts/world-foundry">
-						<i class="fab fa-github"></i>
+						<i class="fab fa-github footer-icon fa-md"></i>
 					</a>
 				</li>
 			</ul>
