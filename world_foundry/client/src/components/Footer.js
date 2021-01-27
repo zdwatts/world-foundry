179bc834fa12d61894c327866dcc50a9ffce2fbb
zacwatts <zdwatts@gmail.com>
2021-01-26 17:03:50 -0700

    wrote basic JSX for footer

--- world_foundry/client/src/components/Footer.js
+++ world_foundry/client/src/components/Footer.js
@@ -1,2 +1,19 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+
+const Footer = () => {
+	return (
+		<footer>
+			<div>
+				<h1>World Foundry</h1>
+			</div>
+			<ul>
+				<li>
+					<NavLink exact to="/" activeClassName="active" />
+				</li>
+			</ul>
+		</footer>
+	);
+};
+
+export default Footer;
