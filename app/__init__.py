8a0773bc3db6e1d13491241221d370237ed3b23c
zacwatts <zdwatts@gmail.com>
2021-06-24 14:22:32 -0600

    switched send from directory return statements

--- app/__init__.py
+++ app/__init__.py
@@ -57,5 +57,5 @@ def inject_csrf_token(response):
 def react_root(path):
     print("path", path)
     if path == "favicon.ico":
-        return app.send_static_file(path)
-    return app.send_static_file('index.html')
+        return send_from_directory("static", "favicon.ico")
+    return send_from_directory("static", "index.html")
