84e40b08330b070ff089f7604b69e24d7c6bf6b6
zacwatts <zdwatts@gmail.com>
2021-06-24 14:11:11 -0600

    fixed root path in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -57,5 +57,5 @@ def inject_csrf_token(response):
 def react_root(path):
     print("path", path)
     if path == "favicon.ico":
-        return send_from_directory("client/build", "favicon.ico")
-    return send_from_directory("client/build", "index.html")
+        return app.send_static_file(path)
+    return app.send_static_file('index.html')
